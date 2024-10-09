c46185531f5a53f6bcc18128178a5f6354ba83fe
Gopikrishna Srinivasan <dhanakrishna94@gmail.com>
2024-10-09 07:28:40 +0530

    updating exercise go files

--- exercises/exercise1.go
+++ exercises/exercise1.go
@@ -19,3 +19,53 @@ func OddNumbers(list []int) (result []int) {
 	}
 	return
 }
+
+func PrimeNumbers(list []int) (result []int) {
+
+	for _, v := range list {
+		if v != 0 && v != 1 && isPrime(v) {
+			result = append(result, v)
+		}
+	}
+
+	return
+}
+
+func PrimeNumbersAndOdd(list []int) (result []int) {
+
+	for _, v := range list {
+		if v != 0 && v != 1 && isPrime(v) && v%2 != 0 {
+			result = append(result, v)
+		}
+	}
+
+	return
+}
+
+func EvenAndMulitplesofFive(list []int) (result []int) {
+
+	for _, v := range list {
+		if v%2 == 0 && v%5 == 0 {
+			result = append(result, v)
+		}
+	}
+	return
+}
+
+func isPrime(n int) (result bool) {
+	incoming := n
+	var times int
+	for ; n > 0; n-- {
+		if incoming%n == 0 {
+			times++
+		}
+	}
+	if times <= 2 {
+		result = true
+	}
+	return
+}
+
+func FunctionHandler(list []int, o func(list []int) []int) []int {
+	return o(list)
+}
--- exercises/exercise1_test.go
+++ exercises/exercise1_test.go
@@ -51,4 +51,96 @@ func TestExercies(t *testing.T) {
 		}
 	})
 
+	t.Run("test Primer function", func(t *testing.T) {
+		list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+		expected := []int{2, 3, 5, 7, 11}
+		expectedLength := len(expected)
+		result := PrimeNumbers(list)
+		actualLength := len(result)
+		t.Log(result)
+		var valid bool = true
+
+		if expectedLength != actualLength {
+			t.Error("result has incorrect number of items")
+		}
+
+		for i := range expected {
+			if expected[i] != result[i] {
+				valid = false
+			}
+		}
+		if !valid {
+			t.Error("result has incorrect items other than expected")
+		}
+	})
+
+	t.Run("test Prime which is ODD function", func(t *testing.T) {
+		list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+		expected := []int{3, 5, 7, 11}
+		expectedLength := len(expected)
+		result := PrimeNumbersAndOdd(list)
+		actualLength := len(result)
+		t.Log(result)
+		var valid bool = true
+
+		if expectedLength != actualLength {
+			t.Error("result has incorrect number of items")
+		}
+
+		for i := range expected {
+			if expected[i] != result[i] {
+				valid = false
+			}
+		}
+		if !valid {
+			t.Error("result has incorrect items other than expected")
+		}
+	})
+
+	t.Run("test Even and Mulitples of 5", func(t *testing.T) {
+		list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 25, 46}
+		expected := []int{10, 20}
+		expectedLength := len(expected)
+		result := EvenAndMulitplesofFive(list)
+		actualLength := len(result)
+		t.Log(result)
+		var valid bool = true
+
+		if expectedLength != actualLength {
+			t.Error("result has incorrect number of items")
+		}
+
+		for i := range expected {
+			if expected[i] != result[i] {
+				valid = false
+			}
+		}
+		if !valid {
+			t.Error("result has incorrect items other than expected")
+		}
+	})
+
+	t.Run("test match ALL the conditions.", func(t *testing.T) {
+		list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 25, 46}
+		expected := []int{10, 20}
+		expectedLength := len(expected)
+		result1 := FunctionHandler(list, OddNumbers)
+		actualLength := len(result1)
+		t.Log(result1)
+		var valid bool = true
+
+		if expectedLength != actualLength {
+			t.Error("result has incorrect number of items")
+		}
+
+		for i := range expected {
+			if expected[i] != result[i] {
+				valid = false
+			}
+		}
+		if !valid {
+			t.Error("result has incorrect items other than expected")
+		}
+	})
+
 }
